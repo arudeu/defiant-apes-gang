cb9cc4a3885c339f603d76fae19e500f55ce9f65
Marc Aldous <devaldous@gmail.com>
2022-06-04 19:56:50 +0800

    Finally!

--- components/head/index.jsx
+++ components/head/index.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import Head from "next/head";
-<<<<<<< HEAD
-=======
-import Script from "next/script";
->>>>>>> 90856d081eb1e5a83710b535bc2c289556a7bebe
 
 const Heads = () => {
   return (
